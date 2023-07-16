b7b4566a56be9ca9c1da5dcb548b9a4135a6436a
ali navidi <ali_navidi24@yahoo.com>
2023-07-16 22:05:51 +0330

    Blogs added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BlogListComponent } from './components/blog-list/blog-list.component';
+import { BlogItemComponent } from './components/blog-item/blog-item.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, BlogListComponent, BlogItemComponent],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/blog-item/blog-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-item',
+  templateUrl: './blog-item.component.html',
+  styleUrls: ['./blog-item.component.css'],
+})
+export class BlogItemComponent {
+  title: string = 'Post';
+}
